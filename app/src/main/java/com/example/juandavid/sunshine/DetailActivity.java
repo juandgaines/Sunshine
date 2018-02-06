4e81c13d3ec8a21956ef0f8e6cbb6f0ae9770f8e
juandgaines <jdavid90.2@gmail.com>
2018-02-05 20:54:22 -0500

    We added string format to formatTemperature in Utility.java through xliff tag and using context

--- app/src/main/java/com/example/juandavid/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/juandavid/sunshine/DetailActivity.java
@@ -222,8 +222,8 @@ public class DetailActivity extends ActionBarActivity {
                 String weatherDescription=data.getString(COL_WEATHER_DESC);
                 boolean isMetric=Utility.isMetric(getActivity());
 
-                String high=Utility.formatTemperature(data.getDouble(COL_WEATHER_MAX_TEMP),isMetric);
-                String low=Utility.formatTemperature(data.getDouble(COL_WEATHER_MIN_TEMP),isMetric);
+                String high=Utility.formatTemperature(getActivity(),data.getDouble(COL_WEATHER_MAX_TEMP),isMetric);
+                String low=Utility.formatTemperature(getActivity(),data.getDouble(COL_WEATHER_MIN_TEMP),isMetric);
 
                 mForecastStr=String.format("%s - %s - %s/%s", dateString,weatherDescription,high,low);
                 TextView detailTextView= (TextView)getView().findViewById(R.id.detail_text);
